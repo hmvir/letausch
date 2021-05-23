0e3326a69b765e2c79388509c768df02ee319984
hmvir <harmanvir.singh@stud.fh-campuswien.ac.at>
2021-05-23 17:33:04 +0200

    Initial commit

---
+++ src/main/java/at/letausch/LetauschApplication.java
@@ -0,0 +1,13 @@
+package at.letausch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetauschApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetauschApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/letausch/LetauschApplicationTests.java
@@ -0,0 +1,13 @@
+package at.letausch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetauschApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
